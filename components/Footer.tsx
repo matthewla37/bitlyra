e288a103ccd069dfa7b1b96115071d03ef48df0d
Matthew Lawyer <lawyermatthew37@outlook.com>
2026-07-06 22:54:03 -0400

    Update footer email address

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -43,9 +43,9 @@ export function Footer() {
                 Email:{" "}
                 <a
                   className="font-medium text-teal-400 transition hover:text-teal-300"
-                  href="mailto:celioalmeida@bitlyra.com"
+                  href="mailto:admin@bitlyra.com"
                 >
-                  celioalmeida@bitlyra.com
+                  admin@bitlyra.com
                 </a>
               </p>
               <div className="mt-5">
